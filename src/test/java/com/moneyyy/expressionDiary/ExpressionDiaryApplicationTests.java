9dcd064defb180f4778581abd265ae8d21d94048
qkrthdud002 <qkrthdud002@gmail.com>
2023-01-09 18:33:29 +0900

    money-diary

---
+++ src/test/java/com/moneyyy/expressionDiary/ExpressionDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneyyy.expressionDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressionDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

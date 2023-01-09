eba6ee4a5edf485b79bb77d881e28407abd5c75d
qkrthdud002 <qkrthdud002@gmail.com>
2023-01-09 18:47:10 +0900

    money-diary

--- src/main/java/com/moneyyy/expressionDiary/ExpressionDiaryApplication.java
+++ src/main/java/com/moneyyy/expressionDiary/ExpressionDiaryApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExpressionDiaryApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ExpressionDiaryApplication.class, args);
 	}

a5de42426bebb79848ce4b1ede647df0922c9215
qkrthdud002 <qkrthdud002@gmail.com>
2023-01-29 23:32:43 +0900

    feat(diary-entity): DTO 작성
    BoardDeleteDTO 작성

---
+++ src/main/java/com/moneyyy/expressionDiary/dto/board/BoardDeleteDTO.java
@@ -0,0 +1,14 @@
+package com.moneyyy.expressionDiary.dto.board;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class BoardDeleteDTO {
+
+    private Long boardId;
+
+}

2ab5d30baf98f89932c4d5b5a6dab0c513ff8ad3
qkrthdud002 <qkrthdud002@gmail.com>
2023-02-03 23:31:40 +0900

    chore(diary-entity): MemberEntity 수정

--- src/main/java/com/moneyyy/expressionDiary/entity/MemberEntity.java
+++ src/main/java/com/moneyyy/expressionDiary/entity/MemberEntity.java
@@ -14,11 +14,12 @@ public class MemberEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // erd 수정하기(필수), Column name "id"로 고치기(보류)
     @Column(name = "member_id")
     private Long memberId;
 
     // 로그인 아이디
-    private int id;
+    private Long id;
 
     // 로그인 비밀번호
     private String password;
